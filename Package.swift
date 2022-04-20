e8f4f5a097425f6246ef72fc756db4156689581a
v.stelmashchuk <volodymyr.stelmashchuk@pm.bet>
2022-04-20 18:15:30 +0300

    Add the checksum to package swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GrowthBookLight",
-            url: "https://github.com/parimatch-tech/growthbook-kotlin/raw/Poc/RemoveNetworkLayer/GrowthBookLight.xcframework.zip"
+            url: "https://github.com/parimatch-tech/growthbook-kotlin/raw/Poc/RemoveNetworkLayer/GrowthBookLight.xcframework.zip",
+            checksum: "5e69bd2ecf26b12127d91af068d24115ec1ec92c28bb9fa93fe8becd10ba3f54"
         ),
     ]
 )
